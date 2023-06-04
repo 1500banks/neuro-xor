3b5a25d5ef5b3dd313f6656a23a056612d91e409
1500banks <1500banks@gmail.com>
2023-06-03 22:08:47 -0400

    project work

--- GidonEtAl/Fig3andS9/x86_64/mod_func.cpp
+++ GidonEtAl/Fig3andS9/x86_64/mod_func.cpp
@@ -16,12 +16,12 @@ extern void _mySyn_reg(void);
 void modl_reg() {
   if (!nrn_nobanner_) if (nrnmpi_myid < 1) {
     fprintf(stderr, "Additional mechanisms from files\n");
-    fprintf(stderr, " \"GidonEtAl/_mod/Traub.mod\"");
-    fprintf(stderr, " \"GidonEtAl/_mod/dCaAP.mod\"");
-    fprintf(stderr, " \"GidonEtAl/_mod/myAMPA.mod\"");
-    fprintf(stderr, " \"GidonEtAl/_mod/myGABA.mod\"");
-    fprintf(stderr, " \"GidonEtAl/_mod/myNMDA.mod\"");
-    fprintf(stderr, " \"GidonEtAl/_mod/mySyn.mod\"");
+    fprintf(stderr, " \"../_mod/Traub.mod\"");
+    fprintf(stderr, " \"../_mod/dCaAP.mod\"");
+    fprintf(stderr, " \"../_mod/myAMPA.mod\"");
+    fprintf(stderr, " \"../_mod/myGABA.mod\"");
+    fprintf(stderr, " \"../_mod/myNMDA.mod\"");
+    fprintf(stderr, " \"../_mod/mySyn.mod\"");
     fprintf(stderr, "\n");
   }
   _Traub_reg();
